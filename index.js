52210124da2e6fbc6e45d800e3552970c370374b
Jerko Steiner <jerko.steiner@gmail.com>
2017-07-04 11:20:44 -0400

    Add backward compatibility for addStream

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 'use strict'
-const iceServers= [{
+var iceServers= [{
    urls: 'stun:stun.l.google.com:19302'
 }]
 
 function createPeer (stream) {
   console.log('createPeer', stream)
 
-	const pc = new RTCPeerConnection({ iceServers })
+	var pc = new RTCPeerConnection({ iceServers: iceServers })
   // create a bogus data channel
   // pc.createDataChannel('a')
+  if (pc.addTrack) {
+    stream.getTracks().forEach(function (track) {
+      console.log('addTrack', track)
+      pc.addTrack(track, stream)
+    })
+  } else {
+    pc.addStream(stream)
+  }
 
-  stream.getTracks().forEach(track => {
-    console.log('addTrack', track)
-    pc.addTrack(track, stream)
-  })
-
-  pc.onicecandidate = e => {
+  pc.onicecandidate = function (e) {
     if (e && e.candidate) {
       console.log('icecandidate', e.candidate.sdpMLineIndex, e.candidate.sdpMid)
     }
@@ -23,7 +26,7 @@ function createPeer (stream) {
 
   return pc.createOffer()
   .then(pc.setLocalDescription.bind(pc))
-  .then(() => 'ok')
+  .then(function () { return 'ok' })
 }
 
 window.navigator.mediaDevices.getUserMedia({ audio: true, video: true })
